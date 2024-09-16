8fbd2d0038c1b8276f4639a82bc89154b2674d5e
juandiegocv27 <juandiegocarballo2701@gmail.com>
2024-09-16 02:44:32 -0600

    feat: improve UI fade

--- Assets/Scripts/UIFade.cs
+++ Assets/Scripts/UIFade.cs
@@ -3,52 +3,44 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class UIFade : MonoBehaviour {
+public class UIFade : MonoBehaviour
+{
 
-    // Singleton instance of UIFade
     public static UIFade instance;
 
-    // Image component used for fading effect
     public Image fadeScreen;
-
-    // Speed of the fade effect
     public float fadeSpeed;
 
-    // Flags to control fade direction
     public bool shouldFadeToBlack;
     public bool shouldFadeFromBlack;
 
     // Use this for initialization
-    void Start () {
-        // Set the singleton instance
+    void Start()
+    {
         instance = this;
 
-        // Ensure this object persists between scene loads
         DontDestroyOnLoad(gameObject);
+
     }
-    
+
     // Update is called once per frame
-    void Update () {
-        // Fade to black if the flag is set
+    void Update()
+    {
+
         if (shouldFadeToBlack)
         {
-            // Gradually increase the alpha of the fadeScreen image to 1 (opaque)
             fadeScreen.color = new Color(fadeScreen.color.r, fadeScreen.color.g, fadeScreen.color.b, Mathf.MoveTowards(fadeScreen.color.a, 1f, fadeSpeed * Time.deltaTime));
 
-            // Check if the fade is complete
-            if(fadeScreen.color.a == 1f)
+            if (fadeScreen.color.a == 1f)
             {
                 shouldFadeToBlack = false;
             }
         }
 
-        // Fade from black if the flag is set
         if (shouldFadeFromBlack)
         {
-            // Gradually decrease the alpha of the fadeScreen image to 0 (transparent)
             fadeScreen.color = new Color(fadeScreen.color.r, fadeScreen.color.g, fadeScreen.color.b, Mathf.MoveTowards(fadeScreen.color.a, 0f, fadeSpeed * Time.deltaTime));
 
-            // Check if the fade is complete
             if (fadeScreen.color.a == 0f)
             {
                 shouldFadeFromBlack = false;
@@ -56,14 +48,13 @@ public class UIFade : MonoBehaviour {
         }
     }
 
-    // Start fading to black
     public void FadeToBlack()
     {
         shouldFadeToBlack = true;
         shouldFadeFromBlack = false;
+
     }
 
-    // Start fading from black
     public void FadeFromBlack()
     {
         shouldFadeToBlack = false;
