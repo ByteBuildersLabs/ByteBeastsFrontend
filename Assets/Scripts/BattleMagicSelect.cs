8254b33ac24e8f0eb79099e68039a1b0c92c0c3b
juandiegocv27 <juandiegocarballo2701@gmail.com>
2024-09-16 01:07:43 -0600

    feat: update battlemagicselect.cs

--- Assets/Scripts/BattleMagicSelect.cs
+++ Assets/Scripts/BattleMagicSelect.cs
@@ -3,50 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class BattleMagicSelect : MonoBehaviour {
+public class BattleMagicSelect : MonoBehaviour
+{
 
-    // Name of the spell associated with this selection
     public string spellName;
-    
-    // MP cost required to cast the spell
     public int spellCost;
-
-    // UI Text components for displaying the spell name and cost
     public Text nameText;
     public Text costText;
 
-    // Called when the script is first initialized
-    void Start () {
-        // Initialization code can go here (currently unused)
-    }
-    
-    // Called once per frame
-    void Update () {
-        // Update logic can go here (currently unused)
+    // Use this for initialization
+    void Start()
+    {
+
     }
 
-    // Called when this magic selection is pressed
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
     public void Press()
     {
-        // Check if the current battler has enough MP to cast the spell
         if (BattleManager.instance.activeBattlers[BattleManager.instance.currentTurn].currentMP >= spellCost)
         {
-            // Hide the magic menu
             BattleManager.instance.magicMenu.SetActive(false);
-            
-            // Open the target selection menu for the spell
             BattleManager.instance.OpenTargetMenu(spellName);
-            
-            // Deduct the MP cost from the current battler's MP
             BattleManager.instance.activeBattlers[BattleManager.instance.currentTurn].currentMP -= spellCost;
-        } 
+        }
         else
         {
-            // Notify the player that they do not have enough MP
+            //let player know there is not enough MP
             BattleManager.instance.battleNotice.theText.text = "Not Enough MP!";
             BattleManager.instance.battleNotice.Activate();
-            
-            // Hide the magic menu
             BattleManager.instance.magicMenu.SetActive(false);
         }
     }
