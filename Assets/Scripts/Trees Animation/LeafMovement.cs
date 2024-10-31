4d1747cdb4d586c65f116ef8239a728fa4dab597
Sebas11042 <sebastianga48@gmail.com>
2024-10-31 13:05:08 -0600

    feat: Tree Animation update

--- Assets/Scripts/Trees Animation/LeafMovement.cs
+++ Assets/Scripts/Trees Animation/LeafMovement.cs
@@ -1,24 +1,36 @@
 using UnityEngine;
 
-public class LeafMovement : MonoBehaviour { 
-
-    public float swayAmount = 0.11f; 
-    public float swaySpeed = 0.98f;   
-    public float noiseScale = 0.2f;
+public class LeafMovement : MonoBehaviour
+{
+    public float swayAmount = 0.11f;      
+    public float swaySpeed = 0.98f;       
+    public float noiseScale = 0.47f;      
+    public float rotationAmount = 2f;     
+    public float rotationSpeed = 0.3f;    
 
     private Vector3 originalPosition;
+    private Quaternion originalRotation;
+    private float timeOffset;             
 
     void Start()
     {
         originalPosition = transform.localPosition;
+        originalRotation = transform.localRotation;
+
+        timeOffset = Random.Range(0f, 10f); 
     }
 
     void Update()
     {
-        
-        float swayX = Mathf.Sin(Time.time * swaySpeed) * swayAmount; 
-        float swayY = Mathf.PerlinNoise(Time.time * noiseScale, 0) * swayAmount; 
+   
+        float swayX = Mathf.Sin((Time.time + timeOffset) * swaySpeed) * swayAmount;
+        float swayY = Mathf.PerlinNoise((Time.time + timeOffset) * noiseScale, 0) * swayAmount;
 
-        transform.localPosition = originalPosition + new Vector3(-swayX, swayY, 0); 
+        
+        transform.localPosition = originalPosition + new Vector3(swayX, swayY, 0);
+
+   
+        float rotationZ = Mathf.Sin((Time.time + timeOffset) * rotationSpeed) * rotationAmount;
+        transform.localRotation = originalRotation * Quaternion.Euler(0, 0, rotationZ);
     }
 }
