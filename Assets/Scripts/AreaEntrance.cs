1078b8b73fa36a8913f6c77fdc9cb5b1fa82afe5
Femi Ojetokun <ojetokunakinwale@gmail.com>
2024-11-23 23:54:48 +0100

    chore: clear entrance data from playerPrefs onApplication quit

--- Assets/Scripts/AreaEntrance.cs
+++ Assets/Scripts/AreaEntrance.cs
@@ -6,47 +6,51 @@ using UnityEngine.SceneManagement;
 /// <summary>
 /// This script manages the entrance to an area in the game.
 /// </summary>
-public class AreaEntrance : MonoBehaviour {
-	/// <value>
+public class AreaEntrance : MonoBehaviour
+{
+    /// <value>
     /// The name of the transition to use when entering this area.
     /// </value>
     public string transitionName;
 
-	// Use this for initialization
-	// test
+    // Use this for initialization
+    // test
 
-	/// <summary>
+    /// <summary>
     /// Initializes the AreaEntrance component.
     /// </summary>
-	void Start () {
-		// Check if the current area transition matches the one set for this entrance
-		if(transitionName == PlayerController.instance.areaTransitionName)
+    void Start()
+    {
+        // Check if the current area transition matches the one set for this entrance
+        if (transitionName == PlayerController.instance.areaTransitionName)
         {
-			// Position the player at the entrance position
+            // Position the player at the entrance position
             PlayerController.instance.transform.position = transform.position;
         }
 
-		// Overwrite player's position if it exists in PlayerPrefs
-		if ( SceneManager.GetActiveScene().name == "Town" && LoadEntrancePositionFromPlayerPrefs() != Vector3.zero) {
+        // Overwrite player's position if it exists in PlayerPrefs
+        if (SceneManager.GetActiveScene().name == "Town" && LoadEntrancePositionFromPlayerPrefs() != Vector3.zero)
+        {
             PlayerController.instance.transform.position = LoadEntrancePositionFromPlayerPrefs();
-			PlayerController.lastHouseEntered = Vector3.zero;
-		}
+            PlayerController.lastHouseEntered = Vector3.zero;
+        }
 
-		// Fade out the black screen
+        // Fade out the black screen
         UIFade.instance.FadeFromBlack();
 
-		// Reset the fading-between-areas flag
+        // Reset the fading-between-areas flag
         GameManager.instance.fadingBetweenAreas = false;
-	}
-	
-	// Update is called once per frame
+    }
 
-	/// <summary>
+    // Update is called once per frame
+
+    /// <summary>
     /// Called every frame after Start().
     /// </summary>
-	void Update () {
-		
-	}
+    void Update()
+    {
+
+    }
 
     public static Vector3 LoadEntrancePositionFromPlayerPrefs()
     {
@@ -62,5 +66,17 @@ public class AreaEntrance : MonoBehaviour {
             Debug.LogWarning($"No Vector3 data found in PlayerPrefs for entrancePosition");
             return Vector3.zero;
         }
+    }
+
+    private void ClearEntrancePositionFromPlayerPrefs()
+    {
+        PlayerPrefs.DeleteKey("entrancePosition_x");
+        PlayerPrefs.DeleteKey("entrancePosition_y");
+        PlayerPrefs.DeleteKey("entrancePosition_z");
+    }
+
+    private void OnApplicationQuit()
+    {
+        ClearEntrancePositionFromPlayerPrefs();
     }
 }
